7c3c6b27e74d7a8b6e1273e3e7e5ebd0520e2bf7
Rusne <rusne.pet"gmail.com>
2020-11-23 15:12:39 +0200

    My first try to start React

---
+++ react.jsx
@@ -0,0 +1,12 @@
+React.createClass = createReactClass;
+var HelloComponent = React.createClass({  
+    render: function() {
+        return React.createElement("div",null,"Hello ",this.props.name    
+            ); 
+         }});
+
+ReactDOM.render(React.createElement( 
+              HelloComponent,  { name: "Jane" }),
+              document.getElementById('root'));
+
+              
